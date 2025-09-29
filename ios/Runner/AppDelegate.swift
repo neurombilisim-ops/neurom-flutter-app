4b4a754f7a106a6dae1a8cfea7cf1a79ec9720a2
neurombilisim-ops <neurombilisim@gmail.com>
2025-09-30 00:08:17 +0300

    Fix iOS build: Remove flutter_downloader import from AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import flutter_downloader
 import GoogleMaps
 
 @UIApplicationMain
@@ -11,13 +10,7 @@ import GoogleMaps
   ) -> Bool {
       GMSServices.provideAPIKey("AIzaSyACtt1uT3NDCjqCAQtJwMDEx66R095M24M")
     GeneratedPluginRegistrant.register(with: self)
-    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-private func registerPlugins(registry: FlutterPluginRegistry) {
-    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
-    }
 }
